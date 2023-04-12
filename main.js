be8f999814221106c1e05666f3f957dd0e7b7fd3
P4anix <pancok_1@yahoo.it>
2023-04-12 17:41:09 +0200

    richiamo il json tramite axios

--- main.js
+++ main.js
@@ -3,5 +3,17 @@
 const { createApp } = Vue;
 
 createApp({
+    data() {
+        return {
+            todos: []
+        }
+    },
+    mounted() {
+        axios.get('todo.json')
+            .then(response => {
+                this.todos = response.data;
+            });
+    }
+
 
 }).mount('#app');
